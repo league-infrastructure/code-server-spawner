774dbab1cf0445da48edfaaa75f89fa7bc9bc681
Eric Busboom <eric@civicknowledge.com>
2025-02-01 21:21:02 -0800

    Refactor CLI context handling and improve container management in routes

--- cspawn/routes/cron.py
+++ cspawn/routes/cron.py
@@ -2,19 +2,12 @@
 
 
 from flask import current_app, jsonify
-from cspawn.app import app, get_db, CI_FILE
-from cspawn.db import update_container_metrics, update_container_state, join_container_info, update_container_info
-from jtlutil.docker.dctl import container_state
-import docker 
-import os
-import json
-from pathlib import Path
+from cspawn.app import app
 
 @app.route("/cron/minutely")
 def minutely():
     current_app.logger.info("Minutely cron job")
-    db = get_db()
-    update_container_info(current_app, get_db())
+    
     return jsonify({"status": "OK"})
 
 @app.route("/cron/hourly")
@@ -28,4 +21,7 @@ def daily():
     current_app.logger.info("Daily cron job")
     
     return jsonify({"status": "OK"})
+
+
+
 
