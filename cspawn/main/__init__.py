150cfcd12d871fa1dc0618ac536eed11e4a6408a
Eric Busboom <eric@civicknowledge.com>
2025-03-14 17:41:53 -0700

    Add SSH tunnel command to Makefile; update devel.env for telemetry URL and interval; enhance YAML configurations for firewall rules and Docker networking

--- cspawn/main/__init__.py
+++ cspawn/main/__init__.py
@@ -1,11 +1,14 @@
-
 import logging
-from typing import cast
-from flask import Blueprint, current_app
+
+from flask import Blueprint
 
 
 main_bp = Blueprint(
-    "main", __name__, static_folder="static", static_url_path="/static/main/", template_folder="templates"
+    "main",
+    __name__,
+    static_folder="static",
+    static_url_path="/static/main/",
+    template_folder="templates",
 )
 
 
